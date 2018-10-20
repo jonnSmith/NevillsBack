39fff792d706de941cfd0d26b72d23220ef52b64
Vard Евгений <Y.Pushkarov@ameria.de>
2018-10-20 16:16:01 +0300

    fix pushes for loop

--- app.js
+++ app.js
@@ -40,15 +40,12 @@ app.use(function(err, req, res, next) {
 
 cron.schedule('0 * * * * *', () => {
     const datestamp = new Date().setSeconds(0,0).toString();
-    console.log('datestamp', datestamp);
     db.readDataByDatestamp(config.routes.events.layer, datestamp).then((snap) => {
-        console.log('result', snap);
-        if(snap && snap.length) {
+        if(snap) {
             for (let e in snap) {
-                console.log('evt', snap[e]);
-                if (snap[e].token !== 'browser') {
-                    console.log('message', {t: [snap[e].token], e: snap[e].title,d:  snap[e].description, id: snap[e].id});
-                    fcm.sendMessage([snap[e].token], snap[e].title, snap[e].description, snap[e].id);
+                let event = snap[e];
+                if (event.token !== 'browser') {
+                    fcm.sendMessage([event.token], event.title, event.description, event.id);
                 }
             }
         }
