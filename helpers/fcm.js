5359c16b072b26698276ae27d68de0508cf2ade1
Yevhen Pushkarov <yevhen.pushkarov@tx.local>
2018-10-22 14:26:20 +0300

    fcm token to key

--- helpers/fcm.js
+++ helpers/fcm.js
@@ -7,6 +7,7 @@ fcmService.fcm = {};
 fcmService.sendMessage = function(token, title, body, url) {
     fcmService.fcm = new FCM(config.firebase.fcm.key);
     let message = {
+        to: token,
         notification: {
             title: title,
             body: body,
@@ -17,11 +18,8 @@ fcmService.sendMessage = function(token, title, body, url) {
         },
         android: {
             ttl: 3600 * 1000,
-            priority: 'high',
-            title: title,
-            body: body,
-        },
-        token: token
+            priority: 'high'
+        }
     };
     console.log('message', message);
     fcmService.fcm.send(message, function(err, response){
