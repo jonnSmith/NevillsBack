ce0b9cbd066571b5ba419e464b0ebd906daa1e8a
Yevhen Pushkarov <yevhen@nearshorepartner.ch>
2020-03-22 12:41:18 +0200

    Revert "Add grouping by timestamp, collapseKey added"
    This reverts commit b7195ba30456da58bff16d32369ea5252f73ba9d.

--- helpers/fcm.js
+++ helpers/fcm.js
@@ -1,6 +1,5 @@
 const FCM = require('fcm-node');
 import config from '../config.json';
-const GROUP_PREFIX = 'channel'
 
 let fcmService = {};
 fcmService.fcm = {};
@@ -17,34 +16,32 @@ fcmService.fcm = {};
  * @param url Push message url data for client app callback
  * @param image Image url for display in push message
  */
-fcmService.sendMessage = function (token, title, body, url, image, datestamp) {
-  fcmService.fcm = new FCM(config.firebase.fcm.key);
-
-  let message = {
-    to: token,
-    priority: "high",
-    restricted_package_name: "",
-    collapseKey: GROUP_PREFIX + datestamp,
-    notification: {
-      click_action: url,
-      body: body,
-      title: title,
-      image: image,
-      sound: "ring.mp3",
-      tag: GROUP_PREFIX + datestamp,
-      android_channel_id: 'rn-push-notification-channel-id',
-    }
-  };
-  console.log('message', message);
-  fcmService.fcm.send(message, function (err, response) {
-    if (err) {
-      console.log('fcm error', err);
-      return err;
-    } else {
-      console.log('fcm response', response);
-      return response;
-    }
-  });
+fcmService.sendMessage = function(token, title, body, url, image) {
+    fcmService.fcm = new FCM(config.firebase.fcm.key);
+    let message = {
+        to: token,
+        priority: "high",
+        restricted_package_name: "",
+        data: {
+            url: url,
+            body: body,
+            title: title,
+            image: image,
+            badge: 1,
+            sound: "ring.mp3",
+            soundName: "ring.mp3"
+        }
+    };
+    console.log('message', message);
+    fcmService.fcm.send(message, function(err, response){
+        if (err) {
+            console.log('fcm error', err);
+            return err;
+        } else {
+            console.log('fcm response', response);
+            return response;
+        }
+    });
 };
 
 module.exports = fcmService;
